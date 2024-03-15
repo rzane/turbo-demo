546e9236ad9e7e6712d58e1c4437a1c8bfd97549
Ray Zane <raymondzane@gmail.com>
2024-03-14 21:34:18 -0400

    Add layout

--- app.rb
+++ app.rb
@@ -1,5 +1,31 @@
 require 'sinatra'
 
 get '/' do
-  'Hello world!'
+  erb 'Hello world'
+end
+
+template :layout do
+  <<~HTML
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Mini Turbo</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+      <script type="module" src="https://cdn.skypack.dev/@hotwired/turbo@7"></script>
+    </head>
+    <body>
+      <main class="container">
+        <nav>
+          <ul>
+            <li><strong>Mini Turbo</strong></li>
+          </ul>
+        </nav>
+
+        <%= yield %>
+      </main>
+    </body>
+    </html>
+  HTML
 end
